c9bebc0e9fa08406ec915abe09bc9e37b54df159
dan <me@dangriffin.online>
2017-01-30 22:22:15 +0000

    Incredibly inefficient and slow multi-threaded sprites

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ cat = new_sprite("cat.png", -150, 100)
 fish = new_sprite("fish.png", 150, 0)
 
 
-@script(cat, fish)
+@script(cat)
 def when_left_arrow_key_pressed(sprite):
     sprite.change_x_by(-10)
 
 
-@script(cat, fish)
+@script(cat)
 def when_right_arrow_key_pressed(sprite):
     sprite.change_x_by(10)
 
 
-@script(cat, fish)
+@script(cat)
 def when_up_arrow_key_pressed(sprite):
     sprite.change_y_by(10)
 
 
-@script(cat, fish)
+@script(cat)
 def when_down_arrow_key_pressed(sprite):
     sprite.change_y_by(-10)
 
 
 @script(fish)
 def when_space_key_pressed(sprite):
-    sprite.move_steps(10)
-    sprite.if_on_edge_bounce()
+    while True:
+        sprite.move_steps(10)
+        sprite.if_on_edge_bounce()
 
 
 click_green_flag()
--- pyscratch.py
+++ pyscratch.py
@@ -1,10 +1,13 @@
 import pygame
 
+from threading import Thread, Condition
+
 sprite_list = []
+event_lock = Condition()
 
 
 def new_sprite(filename, x=0, y=0):
-    sprite = Sprite(filename, x, y)
+    sprite = Sprite(filename, x, y, event_lock)
     sprite_list.append(sprite)
     return sprite
 
@@ -18,7 +21,7 @@ def script(*receivers):
 
 
 class Sprite:
-    def __init__(self, filename, x, y):
+    def __init__(self, filename, x, y, lock):
         self.x = x
         self.y = y
         self.direction = 90
@@ -26,12 +29,38 @@ class Sprite:
         self.centre_x = int(self.image.get_bounding_rect().width/2)
         self.centre_y = int(self.image.get_bounding_rect().height/2)
         self.event_handlers = {}
+        self.event_threads = {}
+        self.lock = lock
+
+    def queue_event(self, event_name):
+
+        if event_name in self.event_threads:
+            return
+
+        def event_handler():
+            with self.lock:
+                self.event_handlers[event_name](self)
+
+                me = self.event_threads[event_name]
+                print(me)
+                del self.event_threads[event_name]
+                del me
+
+        thread = Thread(target=event_handler, daemon=True)
+        self.event_threads[event_name] = thread
+        thread.start()
+
+    def trigger_event(self, event_name):
+        if event_name in self.event_handlers:
+            self.queue_event(event_name)
 
     def change_x_by(self, amount):
         self.x = self.x + amount
+        self.lock.wait()
 
     def change_y_by(self, amount):
         self.y = self.y + amount
+        self.lock.wait()
 
     def move_steps(self, steps):
         if self.direction == 90:
@@ -40,12 +69,16 @@ class Sprite:
         if self.direction == 180:
             self.x = self.x - steps
 
+        self.lock.wait()
+
     def if_on_edge_bounce(self):
         if to_real_coord((self.x, self.y), self.centre_x, self.centre_y)[0] + self.image.get_bounding_rect().width > 700 and self.direction == 90:
             self.direction = 180
 
         if to_real_coord((self.x, self.y), self.centre_x, self.centre_y)[0] <= 0 and self.direction == 180:
             self.direction = 90
+
+        self.lock.wait()
 
 def to_real_coord(coords, offx, offy):
     (x, y) = coords
@@ -82,29 +115,33 @@ def click_green_flag():
     done = False
     while not done:
 
-        for event in pygame.event.get():
-            for sprite in sprite_list:
-                if event.type == pygame.QUIT:
-                    done = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == 32 and "when_space_key_pressed" in sprite.event_handlers:
-                        sprite.event_handlers["when_space_key_pressed"](sprite)
-                    if event.key == 275 and "when_right_arrow_key_pressed" in sprite.event_handlers:
-                        sprite.event_handlers["when_right_arrow_key_pressed"](sprite)
-                    if event.key == 276 and "when_left_arrow_key_pressed" in sprite.event_handlers:
-                        sprite.event_handlers["when_left_arrow_key_pressed"](sprite)
-                    if event.key == 273 and "when_up_arrow_key_pressed" in sprite.event_handlers:
-                        sprite.event_handlers["when_up_arrow_key_pressed"](sprite)
-                    if event.key == 274 and "when_down_arrow_key_pressed" in sprite.event_handlers:
-                        sprite.event_handlers["when_down_arrow_key_pressed"](sprite)
+        with event_lock:
+            for event in pygame.event.get():
+                for sprite in sprite_list:
+                    if event.type == pygame.QUIT:
+                        done = True
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == 32:
+                            sprite.trigger_event("when_space_key_pressed")
+                        if event.key == 275:
+                            sprite.trigger_event("when_right_arrow_key_pressed")
+                        if event.key == 276:
+                            sprite.trigger_event("when_left_arrow_key_pressed")
+                        if event.key == 273:
+                            sprite.trigger_event("when_up_arrow_key_pressed")
+                        if event.key == 274:
+                            sprite.trigger_event("when_down_arrow_key_pressed")
 
-        if not done:
-            screen.fill(WHITE)
+        with event_lock:
+            if not done:
+                screen.fill(WHITE)
 
-            for sprite in sprite_list:
-                screen.blit(sprite.image, to_real_coord([sprite.x, sprite.y], sprite.centre_x, sprite.centre_y))
+                for sprite in sprite_list:
+                    screen.blit(sprite.image, to_real_coord([sprite.x, sprite.y], sprite.centre_x, sprite.centre_y))
 
-            pygame.display.flip()
-            clock.tick(60)
-        else:
-            pygame.quit()
+                pygame.display.flip()
+                clock.tick(60)
+            else:
+                pygame.quit()
+
+            event_lock.notify_all()
