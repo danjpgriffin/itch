1aeab11734b541fcb3436fbccf545170f9a85288
Dan Griffin <dan.griffin@springernature.com>
2017-03-17 16:19:44 +0000

    Added setup.py stuff

---
+++ README.rst
@@ -0,0 +1,4 @@
+Itch
+====
+
+Make it easier for people who have learned scratch to move their projects to python
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='itch',
+    version='0.0.1',
+
+    description='A Scratch-like environment for python',
+    long_description=long_description,
+    url='https://github.com/danjpgriffin/itch',
+    author='Dan Griffin',
+    author_email='itch@xiragon.com',
+    license='GPLv3',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education'
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='scratch education children',
+
+    packages=['itch'],
+
+    install_requires=['greenlet>=0.4',
+                      'pygame>=1.9']
+
+)
+
