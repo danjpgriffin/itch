5a068e10443b05efdebb047020507bf323d8016f
dan <me@dangriffin.online>
2017-04-17 10:58:06 +0100

    Fix metadata

--- setup.py
+++ setup.py
@@ -8,25 +8,25 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='itch',
+    name='itch-framework',
     version='0.0.1',
 
     description='A Scratch-like environment for python',
     long_description=long_description,
     url='https://github.com/danjpgriffin/itch',
     author='Dan Griffin',
-    author_email='itch@xiragon.com',
+    author_email='itch-framework@xiragon.com',
     license='GPLv3',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
 
         'Intended Audience :: Developers',
-        'Intended Audience :: Education'
+        'Intended Audience :: Education',
 
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='scratch education children',
